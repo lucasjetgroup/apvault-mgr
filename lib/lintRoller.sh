d625cfcd2e14773676745a320a181429003b153a
Kermie <kermie@isinthe.house>
2021-07-04 03:55:44 -0700

    Add more definitions, add output newlines

--- lib/lintRoller.sh
+++ lib/lintRoller.sh
@@ -24,7 +24,7 @@ function lintRoller.lintRollBannedKeywords () {
 
 	if [[ $regexes_found -gt 1 ]]; then
 		echo "APVault: Loaded ""$regexes_found"" banned regexes, searching and destroying matches in ""$pathToLintRoll""..."
-		banned_findarg="find "\""$pathToLintRoll"\"" ${banned_findarg::-15}\) -printf 'Lint-rolling %p.'"
+		banned_findarg="find "\""$pathToLintRoll"\"" ${banned_findarg::-15}\) -printf 'Lint-rolling %p.\n'"
 		# handle dry-run
 		if [[ ! $3 = "--dry-run" ]]; then
 			deleteOp=" -delete"
@@ -62,7 +62,7 @@ function lintRoller.lintRollDerivedContent {
 	done < "$derivedContentKeywordsFile"
 
 	if [[ $regexes_found -gt 1 ]]; then
-		screens_findarg="find "\""$pathToLintRoll"\"" ${screens_findarg::-15}\) -printf 'Lint-rolling %p.'"
+		screens_findarg="find "\""$pathToLintRoll"\"" ${screens_findarg::-15}\) -printf 'Lint-rolling %p.\n'"
 		# handle dry-run
 		if [[ ! $3 = "--dry-run" ]]; then
 			screens_findarg+=" -exec rm -rf \"{}\" +"
